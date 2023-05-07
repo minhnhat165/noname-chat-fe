88baf8ba9c56c9ed9921226288515c44eed37791
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-05-07 23:52:35 +0700

    Refactor Layout and Page components to remove unnecessary parameter and import getUserGithub from Layout

--- src/app/(private)/[id]/page.tsx
+++ src/app/(private)/[id]/page.tsx
@@ -1,13 +1,11 @@
+import { getUserGithub } from '../layout';
+
 export interface PageProps {
   params: {
     id: string;
   };
 }
-export const getUserGithub = async (id: string) => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-  const data = await response.json();
-  return data;
-};
+
 const Page = async ({ params }: PageProps) => {
   const user = await getUserGithub(params.id);
 
