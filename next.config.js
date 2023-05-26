c068977a51c5e9213ab3459a8d6339fd9084c50f
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-05-26 15:27:27 +0700

    Add additional image domains to the Next.js configuration file

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
-    images: {
-        domains: ['localhost', 'res.cloudinary.com'],
-    },
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['localhost', 'res.cloudinary.com', 'via.placeholder.com', 'images.unsplash.com'],
+  },
 };
 
 module.exports = nextConfig;
