5fe9fc8dd4376911fa61394e8d12108f3712d45d
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-06-09 07:02:59 +0700

    feat: add user-services api and modify pagination in types

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,8 +1,12 @@
 import { BaseEntity } from './base-entity';
 
+export type Role = 'admin' | 'user';
+export type UserStatus = 'active' | 'banned';
 export type User = {
   username: string;
   email: string;
+  status: UserStatus;
   avatar?: string;
   bio?: string;
+  role: Role;
 } & BaseEntity;
