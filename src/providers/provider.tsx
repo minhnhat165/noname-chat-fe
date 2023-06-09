0d0a55ab9d11796e24acc7e520b79bb26f807c9e
NguyenMinhNhat <nguyenminhnhat301101@gmail.com>
2023-06-09 08:51:56 +0700

    fix: rename file

--- src/providers/provider.tsx
+++ src/providers/provider.tsx
@@ -1,9 +1,9 @@
 import ReactQueryProvider from './react-query-provider';
 
 export interface ProviderProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 export const Provider = ({ children }: ProviderProps) => {
-  return <ReactQueryProvider>{children}</ReactQueryProvider>;
+	return <ReactQueryProvider>{children}</ReactQueryProvider>;
 };
