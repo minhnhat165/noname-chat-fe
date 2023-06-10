3b82163aee47ec988ad4c1ddac6ac2bff6c12ba8
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-06-10 18:26:41 +0700

    refactor(call.ts): change call status options

--- src/types/call.ts
+++ src/types/call.ts
@@ -2,7 +2,7 @@ import { BaseEntity } from './base-entity';
 import { Room } from './room';
 import { User } from './user';
 
-type CallStatus = 'missed' | 'rejected' | 'outgoing';
+type CallStatus = 'ongoing' | 'ended' ;
 
 export type Call = {
   caller: User;
