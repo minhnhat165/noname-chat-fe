cf3945eb7ac0a4eac94031c446d7f21707a00761
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-06-12 01:00:41 +0700

    refactor(call-page): remove unused code and comments

--- src/app/(protected)/call/[id]/page.tsx
+++ src/app/(protected)/call/[id]/page.tsx
@@ -1,11 +1,4 @@
-import { Call, CallStatus } from '@/types/call';
-import { rooms, user } from '@/stores/data-test';
-
 import { CallPage } from '@/components/call/call-page';
-import { Room } from '@/types/room';
-import { getToken } from '@/utils/auth';
-
-// import { CallPage, CallPanel, RepairCallPanel } from '@/components/call';
 
 export interface CallPageProps {
   params: {
@@ -16,41 +9,7 @@ export interface CallPageProps {
   };
 }
 
-async function getRoom(id: string): Promise<Room> {
-  try {
-    const response = await fetch(`${process.env.API_BASE_URL}/calls/${id}`);
-    console.log(response);
-    const res = await response.json();
-    return rooms[0];
-  } catch (error) {
-    throw new Error('Failed to fetch data');
-  }
-}
-
-async function getCall(id: string): Promise<Call> {
-  try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_CLIENT_API_URL}/calls/${id}`);
-    const res = await response.json();
-    console.log(res);
-    return {
-      _id: 1,
-      room: rooms[0],
-      caller: user,
-      rejectedUsers: [],
-      acceptedUsers: [],
-      createdAt: new Date().toISOString(),
-      updatedAt: new Date().toDateString(),
-      status: CallStatus.ONGOING,
-    };
-  } catch (error) {
-    throw new Error('Failed to fetch data');
-  }
-}
-
 const Page = async ({ params, searchParams }: CallPageProps) => {
-  // const callId = searchParams.call_id;
-  // // const room = await getRoom(params.id);
-  // const call = callId ? await getCall(callId) : null;
   return (
     <div className="flex h-screen w-full items-center justify-center">
       <CallPage />
