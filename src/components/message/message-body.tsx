f78721312e31d69d43f3806bd92a738696e98b2e
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-07-02 00:43:29 +0700

    update ui

--- src/components/message/message-body.tsx
+++ src/components/message/message-body.tsx
@@ -76,91 +76,93 @@ const MessageBody = (props: Props) => {
 
   return (
     <div
-      className="mb-1 flex w-[730px] flex-grow flex-col-reverse overflow-y-auto"
+      className="mb-1 flex w-full flex-grow flex-col-reverse items-center overflow-y-auto"
       id="scrollableDiv"
     >
-      {messages.length > 0 ? (
-        <InfiniteScroll
-          dataLength={messages.length || 0}
-          next={fetchNextPage}
-          hasMore={hasNextPage || false}
-          className="!overflow-y-hidden"
-          style={{ display: 'flex', flexDirection: 'column-reverse' }}
-          loader={<Spin size="small" />}
-          inverse={true}
-          scrollableTarget="scrollableDiv"
-        >
-          {messages?.map((message, index) => {
-            return user?._id == (message.sender as User)._id ? (
-              <MyMessage key={message._id} message={message} />
-            ) : (
-              <div key={message._id} className="mr-1 flex items-end">
-                <div className="my-[2px]">
-                  <DisplayName messages={messages} index={index} />
+      <div className="w-full max-w-[730px] px-2">
+        {messages.length > 0 ? (
+          <InfiniteScroll
+            dataLength={messages.length || 0}
+            next={fetchNextPage}
+            hasMore={hasNextPage || false}
+            className="!overflow-y-hidden"
+            style={{ display: 'flex', flexDirection: 'column-reverse' }}
+            loader={<Spin size="small" />}
+            inverse={true}
+            scrollableTarget="scrollableDiv"
+          >
+            {messages?.map((message, index) => {
+              return user?._id == (message.sender as User)._id ? (
+                <MyMessage key={message._id} message={message} />
+              ) : (
+                <div key={message._id} className="mr-1 flex items-end">
+                  <div className="my-[2px]">
+                    <DisplayName messages={messages} index={index} />
 
-                  <div className="flex">
-                    <div className="h-[34px] w-[34px]">
-                      <DisplayAvatar messages={messages} index={index} />
-                    </div>
-                    {message.type === MessageType.TEXT && (
-                      <div className="ml-[10px] mr-2 max-w-[60%] rounded-md bg-white px-3 py-2 ">
-                        {message.content}
+                    <div className="flex">
+                      <div className="h-[34px] w-[34px]">
+                        <DisplayAvatar messages={messages} index={index} />
                       </div>
-                    )}
-                    {message.type === MessageType.IMAGE && (
-                      <div className="ml-[10px] mr-2 max-w-[60%] rounded-md bg-white px-3 py-2 ">
-                        <p>{message.content}</p>
-                        <Image.PreviewGroup
-                          preview={{
-                            onChange: (current, prev) =>
-                              console.log(`current index: ${current}, prev index: ${prev}`),
-                          }}
-                        >
-                          {message.images?.map((image, index) => (
-                            <Image key={index} alt="image" src={image} />
-                          ))}
-                        </Image.PreviewGroup>
-                      </div>
-                    )}
-                    {message.type === MessageType.FILE && (
-                      <div className="ml-[10px] mr-2 max-w-[60%] rounded-md bg-white px-3 py-2">
-                        {!!message.content && (
-                          <div className=" rounded-md bg-white">{message.content}</div>
-                        )}
-                        {message.files?.map((file, index) => (
-                          <div key={index} className=" my-[2px] mr-5  rounded-md bg-white py-1">
-                            <a href={file.link} className="inline-block h-3 w-fit">
-                              <div className="flex items-center">
-                                <FileTextFilled style={{ fontSize: '40px', color: '#3390ec' }} />
-                                <p className="ml-1 font-medium">{file.name}</p>
-                              </div>
-                            </a>
-                          </div>
-                        ))}
-                      </div>
-                    )}
-
-                    {message.type === MessageType.CALL && (
-                      <div className="ml-[10px] mr-2 flex max-w-[60%] items-center rounded-md bg-white px-3 py-2 ">
-                        <div className="mr-2">
-                          <p className="mb-1 font-medium">Message Call</p>
-                          <p className="text-xs text-slate-400">
-                            {formatDateTime(message.createdAt)}
-                          </p>
+                      {message.type === MessageType.TEXT && (
+                        <div className="ml-[10px] mr-2 max-w-[60%] rounded-md bg-white px-3 py-2 ">
+                          {message.content}
                         </div>
+                      )}
+                      {message.type === MessageType.IMAGE && (
+                        <div className="ml-[10px] mr-2 max-w-[60%] rounded-md bg-white px-3 py-2 ">
+                          <p>{message.content}</p>
+                          <Image.PreviewGroup
+                            preview={{
+                              onChange: (current, prev) =>
+                                console.log(`current index: ${current}, prev index: ${prev}`),
+                            }}
+                          >
+                            {message.images?.map((image, index) => (
+                              <Image key={index} alt="image" src={image} />
+                            ))}
+                          </Image.PreviewGroup>
+                        </div>
+                      )}
+                      {message.type === MessageType.FILE && (
+                        <div className="ml-[10px] mr-2 max-w-[60%] rounded-md bg-white px-3 py-2">
+                          {!!message.content && (
+                            <div className=" rounded-md bg-white">{message.content}</div>
+                          )}
+                          {message.files?.map((file, index) => (
+                            <div key={index} className=" my-[2px] mr-5  rounded-md bg-white py-1">
+                              <a href={file.link} className="inline-block h-3 w-fit">
+                                <div className="flex items-center">
+                                  <FileTextFilled style={{ fontSize: '40px', color: '#3390ec' }} />
+                                  <p className="ml-1 font-medium">{file.name}</p>
+                                </div>
+                              </a>
+                            </div>
+                          ))}
+                        </div>
+                      )}
 
-                        <PhoneOutlined style={{ fontSize: '40px', color: '#3390ec' }} />
-                      </div>
-                    )}
+                      {message.type === MessageType.CALL && (
+                        <div className="ml-[10px] mr-2 flex max-w-[60%] items-center rounded-md bg-white px-3 py-2 ">
+                          <div className="mr-2">
+                            <p className="mb-1 font-medium">Message Call</p>
+                            <p className="text-xs text-slate-400">
+                              {formatDateTime(message.createdAt)}
+                            </p>
+                          </div>
+
+                          <PhoneOutlined style={{ fontSize: '40px', color: '#3390ec' }} />
+                        </div>
+                      )}
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </InfiniteScroll>
-      ) : (
-        <div></div>
-      )}
+              );
+            })}
+          </InfiniteScroll>
+        ) : (
+          <div></div>
+        )}
+      </div>
     </div>
   );
 };
