9f14c1f7b7affb95d38bac8c44d78931c4f1922e
Vi Nguyen <khanhvi2941@gmail.com>
2023-07-12 12:50:19 +0700

    update link

--- src/app/banned/page.tsx
+++ src/app/banned/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-const HOME_LINK = 'http://localhost:3000';
+const HOME_LINK = '/';
 
 export default function BannedPage() {
   return (
